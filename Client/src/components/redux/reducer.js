be29c8316122df5fe1ff84643f2ee8384cd717b9
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-09-23 16:48:33 -0300

    Creacion del reducer

---
+++ Client/src/components/redux/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+
+}
+
+
+
+const rootReducer = (state = initialState, {type, payload}) => {
+
+}
+
+
+export default rootReducer;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './components/redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
