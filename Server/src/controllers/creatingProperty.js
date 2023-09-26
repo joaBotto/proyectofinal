77ce07938cb98745f46d27f70ae2f65d303de35f
joaBotto <joaquinbotto2@hotmail.com>
2023-09-26 18:02:19 -0300

    Creacion de rutas get y posts

--- Server/src/controllers/creatingProperty.js
+++ Server/src/controllers/creatingProperty.js
@@ -1,15 +1,15 @@
-const property = require('../models/property');
-
+const property = require("../models/property");
 
 const creatingProperty = async (newProperty) => {
-    const propertyRegistred = await property.findOne({address:newProperty.address})
-    if (propertyRegistred) {
-        throw new Error("the property has already been published")
-    } else {
-        const propertyCreated = await property.create(newProperty)
-        return propertyCreated
-    }
-
+  const propertyRegistred = await property.findOne({
+    address: newProperty.address,
+  });
+  if (propertyRegistred) {
+    throw new Error("The property has already been published");
+  } else {
+    const propertyCreated = await property.create(newProperty);
+    return propertyCreated;
+  }
 };
 
-module.exports = creatingProperty
+module.exports = creatingProperty;
--- Server/src/controllers/creatingUser.js
+++ Server/src/controllers/creatingUser.js
@@ -1,14 +1,13 @@
-const users = require('../models/user');
+const users = require("../models/user");
 
 const creatingUser = async (newUser) => {
-    const userRegistred = await users.findOne({email:newUser.email})
-    if (userRegistred) {
-        throw new Error("user already exists")
-    } else {
-        const userCreated = await users.create(newUser)
-        return userCreated
-    }
-
-}
+  const userRegistred = await users.findOne({ email: newUser.email });
+  if (userRegistred) {
+    throw new Error("User already exists");
+  } else {
+    const userCreated = await users.create(newUser);
+    return userCreated;
+  }
+};
 
 module.exports = creatingUser;
