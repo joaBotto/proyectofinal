21ee7127fabb8d6fb6ca8e6aac80ff47d015fd2c
GuillermoLlanos <guillermollanosp@gmail.com>
2023-09-26 19:51:01 -0300

    creando componentes base navbar footer cards y instalando tailwind

---
+++ Client/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+	content: [
+		'./src/**/*.js', // Archivos JavaScript
+		'./src/**/*.jsx', // Archivos JSX
+		'./src/**/*.html', // Archivos HTML
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
