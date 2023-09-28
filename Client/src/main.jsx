77d256dc2f6c8eb2b1a5079eee330384f1829a6d
Ditrex24 <vasquezsergiodg@gmail.com>
2023-09-27 23:42:42 -0400

    FormUser

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './components/redux/store';
+import store from '../src/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
