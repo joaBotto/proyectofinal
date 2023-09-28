c262726ae335bd287b0d1368b70d850354856ea1
ALINA PICCARDO <piccardoalina@gmail.com>
2023-09-28 17:35:56 -0300

    main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,12 @@
+import "tailwindcss/tailwind.css";
+
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
-import App from "./App.jsx";
-import "tailwindcss/tailwind.css";
+import store from "./components/redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
