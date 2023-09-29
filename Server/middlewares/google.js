e8e59b3a9139cc0582440584c052a5bee1477485
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-09-28 23:50:23 -0300

    problemas con el auth google

--- Server/middlewares/google.js
+++ Server/middlewares/google.js
@@ -7,49 +7,48 @@ const bcrypt = require('bcrypt');
 
 
 passport.use("auth-google",
-    new GoogleStrategy({
+  new GoogleStrategy({
     clientID: "394324508634-dekdd0gut3m661r1krmphogn2ncsqgg9.apps.googleusercontent.com",
     clientSecret: "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",
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     const email = profile.emails[0].value; // Obtener el correo electrónico
-  
+
     // Verificar si el usuario ya existe en la base de datos
     Users.findOne({ email: email }, function(err, existingUser) {
       if (err) {
-        return res.status(500).json({error:"Error in user search"});
+        return cb(err, false); // Manejar errores y fallas de autenticación
       }
-  
-      // Si el usuario ya existe, simplemente retornar el usuario existente
-      if (existingUser) {
-        return res.status(200).json(existingUser);
-      } else {
-  
-      // Si el usuario no existe, generar una contraseña aleatoria y crear un nuevo usuario
-      const randomPassword = Math.random().toString(36).slice(-8); // Genera una cadena de 8 caracteres aleatorios
-      const saltRounds = 12;
 
-      // Hash de la contraseña antes de almacenarla en la base de datos
-      bcrypt.hash(randomPassword, saltRounds, function(err, hashedPassword) {
-        if (err) {
-          return res.status(500).json({error:"Error hashing password"});
-        }
-  
-        // Crea un nuevo usuario en la base de datos con el correo y la contraseña generada
-        Users.create({
-          email: email,
-          password: hashedPassword // Almacena la contraseña hasheada
-        }, function (err, newUser) {
+      // Si el usuario ya existe, retornar el usuario existente
+      if (existingUser) {
+        return cb(null, existingUser); // Autenticación exitosa
+      } else {
+        // Si el usuario no existe, generar una contraseña aleatoria y crear un nuevo usuario
+        const randomPassword = Math.random().toString(36).slice(-8); // Generar una cadena de 8 caracteres aleatorios
+        const saltRounds = 12;
+
+        // Hash de la contraseña antes de almacenarla en la base de datos
+        bcrypt.hash(randomPassword, saltRounds, function(err, hashedPassword) {
           if (err) {
-            return res.status(500).json({error:error.message})
+            return cb(err, false); // Manejar errores y fallas de autenticación
           }
-        
-          // Retorna el usuario creado
-          return res.status(201).json(newUser)
+
+          // Crea un nuevo usuario en la base de datos con el correo y la contraseña generada
+          Users.create({
+            email: email,
+            password: hashedPassword // Almacena la contraseña hasheada
+          }, function (err, newUser) {
+            if (err) {
+              return cb(err, false); // Manejar errores y fallas de autenticación
+            }
+
+            // Retorna el usuario creado
+            return cb(null, newUser); // Autenticación exitosa
+          });
         });
-      });
-    }
+      }
     });
   }
 ));
