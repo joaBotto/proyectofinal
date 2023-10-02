64be17f15334797fe484f2d555850f1e309adfe9
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-02 09:22:44 -0300

    acepto cambios

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/login";
 import Home from "./views/Home/Home";
+<<<<<<< HEAD
 import  CreateProperty  from './components/createProperty/createProperty'
 
+=======
+>>>>>>> 8cb5018 (added logo)
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/login" element={<Login />} />
+<<<<<<< HEAD
 				<Route path="/create" element={<CreateProperty />} />
+=======
+>>>>>>> 8cb5018 (added logo)
 				<Route path="/" element={<Home />} />
 			</Routes>
 		</>
