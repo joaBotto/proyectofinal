16d5d33ecdc81f1325d3729f022f353159449c89
ALINA PICCARDO <piccardoalina@gmail.com>
2023-10-03 15:11:06 -0300

    agregando estilos

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -1,27 +1,27 @@
 import Card from "../Card/Card";
 
-
-export default function Cards({properties}) {
-	console.log("soy prop de cards",properties)
-		return (
-			<div>
-				<h2 className="text-2xl font-extrabold text-gray-900">Properties</h2>
-				<div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-					{properties.length > 0 ? (
-						properties.map((property) => (
-							<Card
-								key={property._id}
-								_id={property._id}
-								title={property.title}
-								description={property.description}
-								price={property.price}
-								images={property.images[0]}
-							/>
-						))
-					) : (
-						<p>Loading...</p>
-					)}
-				</div>
+export default function Cards({ properties }) {
+	console.log("soy prop de cards", properties);
+	return (
+		<div className="p-4">
+			<div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+				{properties.length > 0 ? (
+					properties.map((property) => (
+						<Card
+							key={property._id}
+							_id={property._id}
+							title={property.title}
+							description={property.description}
+							price={property.price}
+							images={property.images[0]}
+							location={property.address.city}
+							bedrooms={property.bedrooms}
+						/>
+					))
+				) : (
+					<p>Loading...</p>
+				)}
 			</div>
-			);
+		</div>
+	);
 }
--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -18,14 +18,27 @@ export default function Home() {
 	const maxPage = Math.ceil(properties.length / perPage);
 
 	return (
-		<div className="h-screen bg-gray-100">
+		<div className="h-screen bg-gray-100 mt-5">
+			<h1 className="absolute text-5xl font-bold text-fuchsia-950 mt-10 top-40 left-7 leading-[1.2]">
+				FIND A HOME THAT
+				<br /> SUITS YOU
+			</h1>
+			<p className="absolute text-lg font-semibold text-white top-[320px] left-7">
+				+200 PROPERTIES
+			</p>
+			<p className="absolute text-lg font-semibold text-white mt-6 top-[320px] left-7">
+				+400 HAPPY CUSTOMERS
+			</p>
+			<Paginado
+				page={page}
+				setPage={setPage}
+				maxPage={maxPage}
+				products={properties}
+			/>
 			<div className="p-4">
 				<Cards properties={properties} />
 			</div>
-			<div className="p-4">
-				<Footer />
-			</div>
-			<Container className="flex justify-center bg-white rounded-lg p-4 shadow-md">
+			<Container className="flex justify-center bg-white rounded-full p-4 shadow-md">
 				<Paginado
 					page={page}
 					setPage={setPage}
@@ -33,6 +46,9 @@ export default function Home() {
 					products={properties}
 				/>
 			</Container>
+			<div className="p-4">
+				<Footer />
+			</div>
 		</div>
 	);
 }
