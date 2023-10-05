9edba996d6ed7bc545dda2421a132aa1460771b7
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-05 20:21:53 -0300

    arreglando dev

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -3,15 +3,10 @@ import {
     GET_PROPERTY_DETAIL,
     CREATE_PROPERTY,
     ADD_USER,
+    FILTERS,
     CLEAN_DETAIL,
     ERROR,
-    SEARCH_PRODUCTO,
-    FILTER_BY_UBICACION,
-    FILTER_BY_PRECIO,
-    FILTER_BY_PILETA,
-    FILTER_BY_FONDO,
-    ORDER_BY_RESENA,
-    FILTER_BY_CATEGORIA,
+    USER_LOGIN
 } from "./actions_types";
 
 const initialState = {
@@ -19,7 +14,6 @@ const initialState = {
   user: {},
   properties:[],
   allproperties: [],
-  filteredData: [],//almacena los inmuebles filtrados
   propertyDetail: {},
   searchTerm: "",
   details: [],
@@ -75,41 +69,11 @@ const rootReducer = (state = initialState, { type, payload }) => {
         propertyDetail: payload,
       };
 
-      
-      case ADD_USER:
-			return {
-				...state,
-				user: payload,
-			};
-
-      case CLEAN_DETAIL:
-                // Lógica para limpiar los detalles del inmueble
-                return {
-                    ...state,
-                    details: [],
-                    loading: false
-                  };
-                  case ERROR:
-            return {
-              ...state,
-              error: payload,
-            };
-
        case ERROR:
       return {
       ...state,
        error:payload,
     };
-
-
-    case SEARCH_PRODUCTO: // Maneja la acción SEARCH_PRODUCTO
-      return {
-        ...state,
-        searchTerm: payload, // Actualiza searchTerm con el valor de la acción
-        selectinmuebles: payload, // Actualiza selectinmuebles si es necesario
-        loading: false,
-        error: "",
-      };
 
     case FILTERS:
       const filterPropertyForType = filterPropertyType(state, payload);
@@ -130,97 +94,12 @@ const rootReducer = (state = initialState, { type, payload }) => {
         ...state,
         user: payload,
       };
-     
-      case FILTER_BY_UBICACION:
-     const ubicacion = payload;
-      const filteredByUbicacion = state.allproperties.filter(
-    (property) => property.address.city === ubicacion
-      );
-  return {
-    ...state,
-    filteredData: filteredByUbicacion,
-  };
-
-
-
-     
-case FILTER_BY_PRECIO:
-  const { minPrice, maxPrice, order } = payload;
-  let filteredByPrecio = [...state.filteredData];
-
-  // Lógica para filtrar inmuebles por precio
-  if (order === 'asc') {
-    filteredByPrecio = filteredByPrecio.sort((a, b) => a.precio - b.precio);
-  } else if (order === 'desc') {
-    filteredByPrecio = filteredByPrecio.sort((a, b) => b.precio - a.precio);
-  }
-
-  filteredByPrecio = filteredByPrecio.filter(
-    (property) => property.precio >= minPrice && property.precio <= maxPrice
-  );
-
-  return {
-    ...state,
-    filteredData: filteredByPrecio,
-  };
-
-
-
-
-     
-     case FILTER_BY_PILETA:
-        // Lógica para filtrar inmuebles por la presencia de una pileta
-        const filteredByPileta = state.inmuebles.filter((properties) => properties.detalle.pileta === action.payload
-          );
-
-          return {
-            ...state,
-            filteredData: filteredByPileta,
-          };
  
-
-      case FILTER_BY_FONDO:
-        // Lógica para filtrar inmuebles por la presencia de un fondo
-        const filteredByFondo = state.producto.filter((properties) => properties.detalle.fondo === action.payload);
-
-
-        return {...state,
-            filteredData: filteredByFondo };
-
-
-
-      case ORDER_BY_RESENA:
-        // Lógica para ordenar inmuebles por reseña
-        const sortByResena = [...state.filteredData].sort((a, b) =>
-        b.puntuacion - a.puntuacion
-      );
-      return {
-        ...state,
-        filteredData: sortByResena,
-      };
-
-      
-        
-          case FILTER_BY_CATEGORIA:
-            // Lógica para filtrar inmuebles por categoría (casa o departamento)
-            const categoria = payload?.type; // Usamos el operador de encadenamiento opcional para manejar posibles valores nulos o indefinidos
-            console.log('Acción en reducer:', type);
-      
-            if (!categoria || categoria === "default") {
-              return {
-                ...state,
-                filteredData: state.allproperties, // Usamos allproperties en lugar de properties para obtener todos los elementos
-              };
-            } else {
-              const filteredByCategoria = state.allproperties.filter(
-                (property) => property.type === categoria
-              );
-              return {
-                ...state,
-                filteredData: filteredByCategoria,
-              };
-            }
-
+      case USER_LOGIN:
+        return{
+          ...state,
+          user:payload
+        }
             
     default:
       return {
