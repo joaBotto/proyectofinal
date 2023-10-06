d2085f540e1f55d5944fa5893af1c6e3191dad0b
ALINA PICCARDO <piccardoalina@gmail.com>
2023-10-06 13:01:48 -0300

    DETAIL

--- Client/src/views/Detail/Detail.jsx
+++ Client/src/views/Detail/Detail.jsx
@@ -30,75 +30,105 @@ const Detail = () => {
 		setIsModalOpen(false);
 	};
 
+	console.log("property detail", property);
+
 	return (
 		<div className="bg-white w-screen h-screen">
 			<NavBarSimple />
 			{property && property.title ? (
 				<div className="w-full px-10 py-20">
-					<div className="w-full flex justify-between">
-						<div>
-							<h1 className="text-5xl font-onest font-extrabold uppercase text-cyan">
-								{property.title}
-							</h1>
-						</div>
-						<div>
-							<button className="flex justify-end text-white bg-transparent rounded-full">
-								<Link
-									to="/"
-									className="mt-1 mr-2 justify-center text-blue font-onest font-semibold"
-								>
-									RETURN
-								</Link>
-								<FontAwesomeIcon
-									icon={faHouse}
-									className="bg-cyan text-blue  py-2 px-2 rounded-full justify-center shadow-lg"
-								/>
-							</button>
-						</div>
-					</div>
-					<div className="w-full flex justify-around pt-10">
-						<div className="w-1/2 ml-2 mr-10">
-							<div className="w-full h-2/3 overflow-hidden rounded-xl shadow-xl">
-								<ImageCarousel images={property.images} />
+					<div>
+						<div className="w-full flex justify-between">
+							<div>
+								<h1 className="text-5xl font-onest font-extrabold uppercase text-cyan">
+									{property.title}
+								</h1>
 							</div>
-							<div className="w-full flex justify-between pt-2">
-								<div className="flex w-full justify-start flex-wrap">
-									{property.images.map((image, index) => (
-										<div className="">
-											<img
-												key={index}
-												src={image.imageUrl}
-												alt={`Thumbnail ${index + 1}`}
-												className="h-[150px] cursor-pointer m-1 filter grayscale hover:grayscale-0 rounded-md"
-												onClick={() => openModal(image)}
-											/>
-										</div>
-									))}
+							<div>
+								<button className="flex justify-end text-white bg-transparent rounded-full">
+									<Link
+										to="/"
+										className="mt-1 mr-2 justify-center text-blue font-onest font-semibold"
+									>
+										RETURN
+									</Link>
+									<FontAwesomeIcon
+										icon={faHouse}
+										className="bg-cyan text-blue  py-2 px-2 rounded-full justify-center shadow-lg"
+									/>
+								</button>
+							</div>
+						</div>
+						<div className="w-full flex justify-around pt-10 overflow-hidden">
+							<div className="w-1/2 ml-2 mr-10">
+								<div className="h-2/3 overflow-hidden rounded-xl shadow-xl">
+									<ImageCarousel images={property.images} />
+								</div>
+								<div className="w-full flex justify-between pt-2">
+									<div className="flex w-full justify-start flex-wrap">
+										{property.images.map((image, index) => (
+											<div className="">
+												<img
+													key={index}
+													src={image.imageUrl}
+													alt={`Thumbnail ${index + 1}`}
+													className="h-[150px] cursor-pointer m-1 filter grayscale hover:grayscale-0 rounded-md"
+													onClick={() => openModal(image)}
+												/>
+											</div>
+										))}
+									</div>
+								</div>
+							</div>
+
+							<div className="w-1/2">
+								<p className="text-blue font-onest font-bold underline pb-3">
+									♥︎SAVE PROPERTY
+								</p>
+								<p className="text-blue font-noto font-bold pb-3">
+									Published X days ago
+								</p>
+								<p className="text-4xl text-blue font-onest font-extrabold py-3">
+									U$D {property.price}
+								</p>
+								<div>
+									<p className="text-3xl text-blue font-onest font-extrabold pt-3">
+										DESCRIPTION
+									</p>
+									<p className="text-md text-blue font-noto text-justify font-light pb-3">
+										{property.description}
+									</p>
+								</div>
+								<div className="w-1/2 border-2 border-cyan rounded-xl mt-3">
+									<p className="text-xl text-blue font-onest font-extrabold pt-3 px-5">
+										PROPERTY OWNER
+									</p>
+									<p className="text-md text-blue font-noto text-justify font-light pb-3 px-5">
+										{property.owner.name}
+									</p>
+								</div>
+								<div className=""></div>
+								<div>
+									<p className="text-xl text-blue font-onest font-extrabold pt-3 px-5">
+										OTHER CHARACTERISTICS
+									</p>
+									<p className="text-md text-blue font-noto text-justify font-light pb-3 px-5"></p>
 								</div>
 							</div>
 						</div>
-
-						<div className="w-1/2">
-							<p className="text-blue font-onest font-bold underline pb-3">
-								♥︎SAVE PROPERTY
-							</p>
-							<p className="text-blue font-noto font-bold pb-3">
-								Published X days ago
-							</p>
-							<p className="text-4xl text-blue font-onest font-extrabold pt-3">
-								U$D {property.price}
-							</p>
-							<div className="propertyOwner"></div>
-							<div className="propertyReview"></div>
-						</div>
+						{isModalOpen && (
+							<ImageGalleryModal
+								images={property.images}
+								selectedImage={selectedImage}
+								onClose={closeModal}
+							/>
+						)}
 					</div>
-					{isModalOpen && (
-						<ImageGalleryModal
-							images={property.images}
-							selectedImage={selectedImage}
-							onClose={closeModal}
-						/>
-					)}
+					<div>
+						<p className="text-4xl text-blue font-onest font-extrabold py-3">
+							LOCATION
+						</p>
+					</div>
 				</div>
 			) : (
 				<p>Loading...</p>
