c49275a2d8c8d3b49cef5ab4396356ef3419f0ca
joaBotto <joaquinbotto2@hotmail.com>
2023-10-07 11:40:31 -0300

    Send email created

--- Client/src/components/Singup/singUp.jsx
+++ Client/src/components/Singup/singUp.jsx
@@ -70,6 +70,31 @@ const SignUpForm = () => {
   const handleValidation = (isValid) => {
     setFormValid(isValid);
   };
+  //----------------------Envio de email----------------------------------
+  const form = useRef();
+
+  const sendEmail = (values) => {
+    const serviceID = "service_phq6wkb";
+    const templateID = "template_d3bst5s";
+    const publicID = "A88VMvhYNS70XCfHm";
+
+    const templateParams = {
+      from_name: values.name,
+      to_email: values.email,
+      subject: "Inmuebles360 :)",
+      message: "Bienvenido a nuestra plataforma!",
+    };
+
+    emailjs
+      .sendForm(serviceID, templateID, templateParams, form.current, publicID)
+      .then((result) => {
+        alert("Sign Up success: ", result);
+      })
+      .catch((error) => {
+        alert("Something was wrong: ", error);
+      });
+  };
+  //----------------------------------------------------------------------
 
   const uploadImagesToCloudinary = async (file) => {
     const formData = new FormData();
@@ -106,6 +131,7 @@ const SignUpForm = () => {
       }
 
       await dispatch(addUser(values));
+      sendEmail(values);
 
       // Después de que el usuario se haya creado con éxito, establece userCreated en true
       setUserCreated(true);
