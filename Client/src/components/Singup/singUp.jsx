f30dce553a86e0432f22b9ec049c209a5b9d3ede
joaBotto <joaquinbotto2@hotmail.com>
2023-10-10 19:03:11 -0300

    Envio de email casi listo

--- Client/src/components/Singup/singUp.jsx
+++ Client/src/components/Singup/singUp.jsx
@@ -73,6 +73,31 @@ const SignUpForm = () => {
     setFormValid(isValid);
   };
 
+  //----------------------Envio de email----------------------------------
+  const form = useRef();
+
+  const sendEmail = (values) => {
+    const serviceID = "service_phq6wkb";
+    const templateID = "template_d3bst5s";
+    const publicID = "A88VMvhYNS70XCfHm";
+
+    const templateParams = {
+      from_name: values.name,
+      to_email: values.email,
+      subject: "Inmuebles360 :)",
+      message: "Bienvenido a nuestra plataforma!",
+    };
+
+    emailjs
+      .sendForm(serviceID, templateID, templateParams, form.current, publicID)
+      .then((result) => {
+        alert("Sign Up success: ", result);
+      })
+      .catch((error) => {
+        alert("Something was wrong: ", error);
+      });
+  };
+  //----------------------------------------------------------------------
   const uploadImagesToCloudinary = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
