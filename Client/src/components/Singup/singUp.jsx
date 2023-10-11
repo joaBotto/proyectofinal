88e5973047f802e1dfab25c93aae095b3ccda397
joaBotto <joaquinbotto2@hotmail.com>
2023-10-11 20:21:39 -0300

    Updated changes

--- Client/src/components/Singup/singUp.jsx
+++ Client/src/components/Singup/singUp.jsx
@@ -74,29 +74,29 @@ const SignUpForm = () => {
   };
 
   //----------------------Envio de email----------------------------------
-  const form = useRef();
+  // const form = useRef();
 
-  const sendEmail = (values) => {
-    const serviceID = "service_phq6wkb";
-    const templateID = "template_d3bst5s";
-    const publicID = "A88VMvhYNS70XCfHm";
+  // const sendEmail = (values) => {
+  //   const serviceID = "service_phq6wkb";
+  //   const templateID = "template_d3bst5s";
+  //   const publicID = "A88VMvhYNS70XCfHm";
 
-    const templateParams = {
-      from_name: values.name,
-      to_email: values.email,
-      subject: "Inmuebles360 :)",
-      message: "Bienvenido a nuestra plataforma!",
-    };
+  //   const templateParams = {
+  //     from_name: values.name,
+  //     to_email: values.email,
+  //     subject: "Inmuebles360 :)",
+  //     message: "Bienvenido a nuestra plataforma!",
+  //   };
 
-    emailjs
-      .sendForm(serviceID, templateID, templateParams, form.current, publicID)
-      .then((result) => {
-        alert("Sign Up success: ", result);
-      })
-      .catch((error) => {
-        alert("Something was wrong: ", error);
-      });
-  };
+  //   emailjs
+  //     .sendForm(serviceID, templateID, templateParams, form.current, publicID)
+  //     .then((result) => {
+  //       alert("Sign Up success: ", result);
+  //     })
+  //     .catch((error) => {
+  //       alert("Something was wrong: ", error);
+  //     });
+  // };
   //----------------------------------------------------------------------
   const uploadImagesToCloudinary = async (file) => {
     const formData = new FormData();
