948eb10fc4da1f445f1aef646350b6ce70be1fdc
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-13 10:47:07 -0300

    arreglo alluser

--- Server/src/controllers/getUsers.js
+++ Server/src/controllers/getUsers.js
@@ -1,11 +1,17 @@
 const Users = require('../models/user')
 
 const getAllUsers = async () => {
-    const allUsers = await Users.find().lean();
+
+	try {
+		const allUsers = await Users.find().lean();
 	if (allUsers) {
 		return allUsers;
 	} else {
 		throw new Error("There are no users");
+	}	
+	} catch (error) {
+		throw new Error("Error fetching users: " + error.message)
 	}
+    
 }
 module.exports = getAllUsers;
