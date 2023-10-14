87a1eea9d28979d1ab75b3751a0ac8debea333bf
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-13 23:46:08 -0300

    arreglo all users app

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -18,7 +18,7 @@ import BookingSystem from "./views/Reservations/Reservations";
 import Postuser from "./components/propertiesAdmin/propertyUser";
 import PaymentForm from "./components/PaymentForm/PaymentForm";
 import GestionUser from "./views/gestionUser/GestionUser";
-import { allUsers } from "./components/admin/allUsers/allUsers";
+import { AllUsers } from "./components/admin/allUsers/allUsers";
 import EditAccount from "./components/propertiesAdmin/personalEdit";
 
 function App() {
@@ -45,7 +45,7 @@ function App() {
         <Route path="/error404" element={<Error404 />} />
         <Route path="/loginadmin" element={<LoginAdmin />} />
         <Route path="/admin/property/:id" element={<EditPropertyFromAdmin />} />
-        <Route path="/admin/users" element={<allUsers />} />
+        <Route path="/admin/users" element={<AllUsers />} />
         <Route path="/detail/:id/reservations" element={<BookingSystem />} />
         <Route path="/postUser" element={<Postuser />} />
         <Route path="/checkout" element={<PaymentForm />} />
