9fc6a3445d4136353e926c0c02f54789ae428f40
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-20 01:48:05 -0300

    arreglando server

--- Server/src/routes/propertiesRouter.js
+++ Server/src/routes/propertiesRouter.js
@@ -9,13 +9,8 @@ const {
 	editPropertyAvailability,
 } = require("../handlers/propertyHandler");
 
-<<<<<<< HEAD
 propertiesRouter.put("/", editPropertyHandler);
 propertiesRouter.put("/:id", editPropertyAvailability);
-=======
-
-propertiesRouter.put("/",editPropertyHandler)
->>>>>>> 2d72d61 (terminando la auth por terceros)
 propertiesRouter.get("/", getPropertiesHandler);
 propertiesRouter.get("/:id", getPropertyByIdHandler);
 propertiesRouter.post("/", creatingPropertyHandler);
@@ -30,14 +25,7 @@ propertiesRouter.post("/", creatingPropertyHandler);
           res.status(401).json({ message: "No autorizado" });
         }
       }, */
-<<<<<<< HEAD
-
-module.exports = {
-	propertiesRouter,
-};
-=======
       
 module.exports = { 
         propertiesRouter
         };
->>>>>>> 2d72d61 (terminando la auth por terceros)
