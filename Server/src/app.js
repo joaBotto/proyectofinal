4fbe5a77d4f2a5ecebd5641baec699cc0310188c
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-23 15:11:51 -0300

    config cors

--- Server/src/app.js
+++ Server/src/app.js
@@ -23,7 +23,7 @@ const storage = multer.memoryStorage(); // Almacenamiento en memoria (puedes cam
 const server = express();
 
 const corsOptions = {
-	origin: "*", // Permite solicitudes desde este origen
+	origin: "http://localhost:3000", // Permite solicitudes desde este origen
 	methods: 'GET, POST, OPTIONS, PUT, DELETE',
 	allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept', // Solo permite estos encabezados
 	credentials: true, // Permite enviar cookies
