199093bda96e06033589befb9947759ba1dccd8c
ALINA PICCARDO <piccardoalina@gmail.com>
2023-10-23 17:52:32 -0300

    styles nuevos

--- Client/src/components/Paginado/paginado.jsx
+++ Client/src/components/Paginado/paginado.jsx
@@ -2,35 +2,28 @@ import React, { useEffect } from "react";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
+export default function PaginationButtons({ page, setPage, maxPage, data }) {
+	const handleChange = (event, value) => {
+		setPage(value);
+	};
 
+	useEffect(() => {
+		setPage(1);
+	}, [data, setPage]);
 
-
-export default function PaginationButtons({
-  page,
-  setPage,
-  maxPage,
-  data,
-}) {
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
-
-  useEffect(() => {
-    setPage(1);
-  }, [data, setPage]);
-
-  return (
-    <div className="flex items-center justify-center space-x-2">
-      <Stack spacing={2}>
-        <Pagination
-          count={maxPage}
-          onChange={handleChange}
-          defaultPage={1}
-          page={page}
-          showFirstButton
-          showLastButton
-        />
-      </Stack>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center space-x-2">
+			<Stack spacing={2}>
+				<Pagination
+					count={maxPage}
+					onChange={handleChange}
+					defaultPage={1}
+					page={page}
+					showFirstButton
+					showLastButton
+					className="text-cyan"
+				/>
+			</Stack>
+		</div>
+	);
 }
--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -1,25 +1,21 @@
-
-const SearchBar = ({handleChange, search}) => {
-
-
+const SearchBar = ({ handleChange, search }) => {
 	return (
 		<div className="w-1/3 rounded-full relative top-4">
-			<form >
+			<form className="flex flex-row">
 				<input
-					className="font-onest text-blue px-3 py-1 w-full border-blue border-b-4 border-r-2 rounded-full shadow-md"
+					className="font-onest text-blue px-3 py-2 w-full border-blue border-b-4 border-r-2 rounded-full shadow-md"
 					type="text"
 					placeholder="Search..."
 					name="searchBar"
 					value={search}
 					onChange={handleChange}
 				/>
-				
 				<button
 					name="searchBar"
 					type="button"
 					onClick={handleChange}
-					value = ""
-					className="bg-blue rounded-full py-1 px-2 ml-2 text-white hover:bg-cyan"
+					value=""
+					className="font-onest text-blue bg-cyan hover:bg-opacity-50 border-blue border-2 px-3 py-1 rounded-full text-sm absolute right-2 bottom-2"
 				>
 					x
 				</button>
