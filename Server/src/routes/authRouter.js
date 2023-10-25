b8fa3033dd6b2231848bbdeab612cddc2c59215d
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-25 13:16:18 -0300

    comente logica de authRouter de login para que no se pise con la que anda

--- Server/src/routes/authRouter.js
+++ Server/src/routes/authRouter.js
@@ -24,16 +24,6 @@ authRouter.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
-
-/* authRouter.post("/login", passport.authenticate("local", {
-  failureRedirect: "http://localhost:3000/login",
-}), (req,res ) => {
-  console.log("La autenticación tuvo éxito");
-  console.log("Usuario autenticado:", req.user);
-  res.redirect("http://localhost:3000");
-}); */
-
-
 authRouter.get("/google", passport.authenticate("auth-google", {scope:['profile','email']}))
 
 authRouter.get("/google/callback", passport.authenticate("auth-google", {
@@ -50,11 +40,11 @@ authRouter.get("/user", ensureAuthenticated, (req,res) => {
   res.status(200).json({user})
 })
 
-authRouter.post("/login", passport.authenticate("local", {
+/* authRouter.post("/login", passport.authenticate("local", {
   successRedirect: "http://localhost:3000", // Redirige en caso de autenticación exitosa
   failureRedirect: "http://localhost:3000/login",
 }));
-
+ */
 
 /* router.get("/logout", (req, res) => {
   if (req.isAuthenticated()) {
