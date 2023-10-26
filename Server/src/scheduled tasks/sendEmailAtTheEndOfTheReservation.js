127fa23162f3d51a168bb047be329b32e0dda0af
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-26 13:16:05 -0300

    comente console.log de sendEmail

--- Server/src/scheduled tasks/sendEmailAtTheEndOfTheReservation.js
+++ Server/src/scheduled tasks/sendEmailAtTheEndOfTheReservation.js
@@ -10,13 +10,12 @@ cron.schedule("00 23 * * *", async () => { // minutos-hora
     const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
     const year = currentDate.getFullYear();
     currentDate = `${day}-${month}-${year}`;
-    console.log("SOY CURRENT", currentDate);
 
     const bookings = await booking.find({ endDate: currentDate });
     for (const booking of bookings) {
       const reviewLink = `http://localhost:3000/reviews/${booking._id}`; 
-  console.log("SOY EL MAIL",booking.guest.email)
-  console.log("SOY EL ID DE BOOKING", booking._id)
+  // console.log("SOY EL MAIL",booking.guest.email)
+  // console.log("SOY EL ID DE BOOKING", booking._id)
       const mailOptions = {
         from: process.env.EMAIL,
         to: `${booking.guest.email}`,
