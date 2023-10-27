bd4524bf693c887f91516d2cd13ee9280f15a2be
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-10-26 21:55:18 -0300

    agregando role al user

--- Server/src/handlers/userHandler.js
+++ Server/src/handlers/userHandler.js
@@ -41,7 +41,8 @@ const createUserHandler = async (req, res) => {
       city,
       address,
       phoneNumber,
-      active:true
+      active:true,
+      role:"user"
     };
     console.log("Recibida solicitud para crear usuario:", user);
     if (
